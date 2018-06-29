1741f5cd2c9e7d4f1a0f0a8cfe4822e321653411
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-29 09:21:00 -0700

    initialize class properties and begin writing first test method

---
+++ WordCounter.Tests/RepeatCounterTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class RepeatCounterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
