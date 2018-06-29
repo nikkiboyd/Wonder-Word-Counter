e9a414d31c0f231f73d9d7ef51cd980c53d85d34
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-29 09:36:38 -0700

    work on fixing greyed out namespace preventing tests and solution from linking

--- WordCounter/RepeatCounter.cs
+++ WordCounter/RepeatCounter.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace WordCounter
 {
     public class RepeatCounter
     {
-        public RepeatCounter()
+        private string _wordToCheck;
+        private List<string> _sentenceToCheck;
+        private List<string> _verifiedMatches;
+
+        public RepeatCounter(string wordToCheck, List<string> sentenceToCheck)
         {
+            _wordToCheck = wordToCheck;
+            _sentenceToCheck = sentenceToCheck;
         }
+
+        public string GetWordToCheck()
+        {
+            return _wordToCheck;
+        }
+
+
     }
 }
