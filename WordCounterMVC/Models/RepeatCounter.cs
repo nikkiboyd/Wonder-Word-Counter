c3b6127f0160ebd4a6ecaf634c15e94409ca077e
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-06 08:52:26 -0700

    fix file structure so that page loads on build

---
+++ WordCounterMVC/Models/RepeatCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace WordCounter
+{
+    public class RepeatCounter
+    {
+        private string _wordToCheck;
+        private string _sentenceToCheck;
+
+        public RepeatCounter(string wordToCheck, string sentenceToCheck)
+        {
+            _wordToCheck = wordToCheck;
+            _sentenceToCheck = sentenceToCheck;
+        }
+
+        public int CountOccurences(string userWord, string userSentence)
+        {
+            string[] wordArray = userSentence.Split(new char[] { ' ', '.', ',', ';', ':', '?', '!'});
+            var findMatches = from word in wordArray
+                              where word.ToLower() == userWord.ToLower()
+                              select word;
+            
+            int wordFrequency = findMatches.Count();
+            return wordFrequency;
+        }
+    }
+}
